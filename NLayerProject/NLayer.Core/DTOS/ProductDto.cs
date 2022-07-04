acf18aea891ca37d2f0cfe71bea95c5cfb8f5a0c
TanjuSimsek <tanju.simsek@yandex.com>
2022-07-04 10:07:02 +0300

    dtos-class-added

--- NLayerProject/NLayer.Core/DTOS/ProductDto.cs
+++ NLayerProject/NLayer.Core/DTOS/ProductDto.cs
@@ -1,22 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NLayer.Core
+namespace NLayer.Core.DTOS
 {
-    public class Product:BaseEntity
+    public  class ProductDto:BaseDto
     {
         public string Name { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
 
         public int CategoryId { get; set; }
-
-        public Category Category { get; set; }
-
-        public ProductFeature ProductFeature { get; set; }
     }
 }
--- NLayerProject/NLayer.Repository/AppDbContext.cs
+++ NLayerProject/NLayer.Repository/AppDbContext.cs
@@ -6,7 +6,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using NLayer.Core;
+using NLayer.Core.Models;
 
 namespace NLayer.Repository
 {
